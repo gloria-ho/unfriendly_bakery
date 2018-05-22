0b34556a5b8512bb0c3895d6299dd7d69298276f
silverdragonia <silverdragonia@gmail.com>
2018-05-22 12:47:08 -0400

    added image modals

--- public/index.js
+++ public/index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+$('#img_modal').on('show.bs.modal', function (event) {
+  let button = $(event.relatedTarget)
+  let item = button.data('info')
+  let modal = $(this)
+  $('#modal-img').attr("src", item)
+})
