f92d1711e68aa9cda979bc000450087d9a07bea8
Gloria H <silverdragonia@gmail.com>
2018-06-26 10:22:14 -0400

    heroku deployment update

--- app.rb
+++ app.rb
@@ -5,71 +5,69 @@ require './cakes'
 require './muffins'
 
 class App < Sinatra::Base
-  get "/" do
-    "Hello world!"
+
+  sugar = @cookie_sugar
+  chocolate = @cookie_chocolate
+  caramel = @cookie_caramel
+  chocolate_cake = @cake_chocolate
+  iced = @cake_iced
+  pie = @cake_pie
+  almond = @cupcake_almond
+  pretzel = @cupcake_pretzel
+  red_velvet = @cupcake_red_velvet
+
+  get ('/') do
+    erb(:index)
   end
-end
 
-sugar = @cookie_sugar
-chocolate = @cookie_chocolate
-caramel = @cookie_caramel
-chocolate_cake = @cake_chocolate
-iced = @cake_iced
-pie = @cake_pie
-almond = @cupcake_almond
-pretzel = @cupcake_pretzel
-red_velvet = @cupcake_red_velvet
+  get ('/about') do
+    erb(:about)
+  end
 
-get ('/') do
-  erb(:index)
-end
+  get('/cookies') do
+    @sugar = sugar
+    @chocolate = chocolate
+    @caramel = caramel
+    erb(:cookies)
+  end
 
-get ('/about') do
-  erb(:about)
-end
+  get('/cakes') do
+    @chocolate = chocolate_cake
+    @iced = iced
+    @pie = pie
+    erb(:cakes)
+  end
 
-get('/cookies') do
-  @sugar = sugar
-  @chocolate = chocolate
-  @caramel = caramel
-  erb(:cookies)
-end
+  get('/muffins') do
+    @almond = almond
+    @pretzel = pretzel
+    @red_velvet = red_velvet
+    erb(:muffins)
+  end
 
-get('/cakes') do
-  @chocolate = chocolate_cake
-  @iced = iced
-  @pie = pie
-  erb(:cakes)
-end
+  get('/subscribe') do
+    erb(:subscribe)
+  end
 
-get('/muffins') do
-  @almond = almond
-  @pretzel = pretzel
-  @red_velvet = red_velvet
-  erb(:muffins)
-end
+  post('/signup') do
+    @email = params[:email]
+    mg_client = Mailgun::Client.new (ENV['MAILGUN_API_KEY'])
+    message_params =  { from: ENV['WDEMAIL'],
+                        to:   @email,
+                        subject: 'Welcome to the Unfriendly Bakery!',
+                        html:  erb(:mail_template, layout: false)
+                      }    
+    mg_client.send_message ENV['MAILGUN_API_DOMAIN'], message_params
+    redirect 'subscribed'
+  end
 
-get('/subscribe') do
-  erb(:subscribe)
-end
+  get('/subscribed') do
+    @email = params[:email]
+    erb(:subscribed)
+  end
 
-post('/signup') do
-  @email = params[:email]
-  mg_client = Mailgun::Client.new (ENV['MAILGUN_API_KEY'])
-  message_params =  { from: ENV['WDEMAIL'],
-                      to:   @email,
-                      subject: 'Welcome to the Unfriendly Bakery!',
-                      html:  erb(:mail_template, layout: false)
-                    }    
-  mg_client.send_message ENV['MAILGUN_API_DOMAIN'], message_params
-  redirect 'subscribed'
-end
+  get('/visit') do
+    erb(:visit)
+  end
 
-get('/subscribed') do
-  @email = params[:email]
-  erb(:subscribed)
-end
-
-get('/visit') do
-  erb(:visit)
 end
